1685960a18a1d07cff4a56694a6b6f231604363a
thunpanhasuon <thunpanhasuon@gmail.com>
2026-01-11 10:53:18 +0700

    Add files via upload

---
+++ kmath.c
@@ -0,0 +1,70 @@
+#include "../include/kunmath/kmath.h"
+
+static void caluclate_cordic(double angle, double *cos, double* sin) {
+    double x = CORDIC_K;  
+    double y = 0.0f; 
+    double theta = 0.0f;  
+    
+    for (int i = 0; i <  CORDIC_ITERATIONS; i++) {
+        double pow2 = 1.0f / (1 << i); 
+
+        if (theta < angle) {
+            double next_x = x - y * pow2;
+            double next_y = y + x * pow2;
+            x = next_x; 
+            y = next_y; 
+
+            theta += cordic_angles[i]; 
+
+        } 
+        else {
+            double next_x = x + y * pow2;
+            double next_y = y - x * pow2;
+            x = next_x; 
+            y = next_y; 
+
+            theta -= cordic_angles[i]; 
+
+        }
+    }
+
+    *cos = x; 
+    *sin = y; 
+
+} 
+double ksin(double angle) {
+
+    double reduce_angle = angle; 
+    double sigh; 
+
+    if (reduce_angle < 0) {
+        reduce_angle += 2 * M_PI; 
+    }
+    /* sin(180 - θ) = sin(θ) II */ 
+    else if (reduce_angle > M_PI/2 && reduce_angle <= M_PI) {
+        reduce_angle = M_PI - reduce_angle; 
+    }
+    /* sin(θ  - 180) = -sin(θ) III */
+    else if (reduce_angle > M_PI && reudce_angle <= 3*M_PI/22) {
+        reduce_angle =  reduce_angle - M_PI; 
+        sigh = -1.0f; 
+    }
+    /* sin(360 - θ) IV */  
+    else if (reduce_angle >= 2*M_PI) {
+        reduce_angle = 2*M_PI - reduce_angle; 
+        sigh = -1.0f; 
+    }
+    double cos, sin; 
+
+    caluclate_cordic(angle, &cos, &sin); 
+
+    return sign * sin; 
+} 
+
+double kcos(angle) {
+    return ksin(M_PI /2 - angle); 
+}
+
+double ktan(angle) { 
+    return ksin(angle) / kcos(angle); 
+}
---
+++ kmath.h
@@ -0,0 +1,46 @@
+#ifndef KMATH_H
+#define KMATH_H
+
+#include <errno.h>
+#include <math.h>
+
+static const double cordic_angles[] = {
+    0.7853981633974483,    
+    0.4636476090008061,    
+    0.24497866312686414,   
+    0.12435499454676144,   
+    0.06241880999595735,   
+    0.031239833430268277,  
+    0.015623728620476831,  
+    0.007812341060101111,  
+    0.0039062301319669718, 
+    0.0019531225164788188, 
+    0.0009765621895593195, 
+    0.0004882812111948983, 
+    0.00024414062014936177, 
+    0.00012207031189367021, 
+    0.00006103515617420877  
+};
+
+#define CORDIC_K 0.6072529350088812561694
+#define CORDIC_ITERATIONS 15
+#define max(A, B) ((A) > (B) ? (A) : (B)) 
+
+/* trigonometric function */
+static void caluclate_cordic(double angle, double *cos, double* sin); 
+double ksin(double angle); 
+double kcos(double angle); 
+double ktan(double angle); 
+
+/* for calucalating area and valume */
+double log(double arg); 
+double ln(double arg); 
+double pow(double x, double y);
+double sqrt(double arg); 
+
+/* hyperbolic function */
+double kshx(double x); 
+double kchx(double x); 
+double thx(double x); 
+
+#endif 
