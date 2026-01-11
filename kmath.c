374fddd8885bb48481a1a35e788a1a8de470826e
thunpanhasuon <thunpanhasuon@gmail.com>
2026-01-11 13:19:53 +0700

    Update kmath.c

--- kmath.c
+++ kmath.c
@@ -72,10 +72,10 @@ double ktan(angle) {
     double t = (c == 0) ? 0 : S / C; 
     return t; 
 }
-double d2rad(double rad) {
+double rad2d(double rad) {
     return 180.0f/M_PI * rad; 
 }
-double rad2d(double dergee) {
+double d2rad(double dergee) {
     return M_PI/180.0f * dergee; 
 }
 /* it not an accurate guess but it approximated pretty well */
